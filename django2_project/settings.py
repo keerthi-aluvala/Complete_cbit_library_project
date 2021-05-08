f37ead85818849b88ad78bfb55bbd878fa0b38e1
Keerthi Aluvala <keerthialuvala7@gmail.com>
2021-05-08 22:53:37 +0530

    added redirect path of subjectview_sem1.html

--- django2_project/settings.py
+++ django2_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +125,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+libraryitsem1_REDIRECT_URL = reverse_lazy('library:libraryitsems')
+libraryitsem1_URL = reverse_lazy('library:libraryitsem1')
